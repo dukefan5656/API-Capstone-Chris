9b9ad6c6f20d37e70b6cc0907a57246ddf183d30
Chris Rudder <cbrudder84@gmail.com>
2018-10-30 12:08:01 -0400

    finished all API requests

--- scripts/api.js
+++ scripts/api.js
@@ -4,8 +4,8 @@ const API = (function () {
   
   const youtube_Key = 'AIzaSyBhyLNH7AaEkj0c6aE5FB8NMrirtnyWNhg';
   let youtubeEndpoint = 'https://www.googleapis.com/youtube/v3/search';
-  let flickrEndpoint = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
-  let redditEndpoint = 'http://www.reddit.com/r/all/search.json?';
+  let flickrEndpoint = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+  let redditEndpoint = 'https://www.reddit.com/r/all/search.json?';
   let wikiEnpoint = 'https://www.wikipedia.org/w/api.php?callback=?';
 
   const getWiki = function(searchTerm, callback) {
